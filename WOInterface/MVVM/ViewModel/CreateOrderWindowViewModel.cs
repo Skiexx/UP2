ad9d520947b89bb412321acf9b93332cebe913f0
gr602_banvl <krupashow@ya.ru>
2022-03-12 06:03:06 +0700

    Item Source Error at DataGrid CreateOrder.xaml

--- WOInterface/MVVM/ViewModel/CreateOrderWindowViewModel.cs
+++ WOInterface/MVVM/ViewModel/CreateOrderWindowViewModel.cs
@@ -1,7 +1,36 @@
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using Microsoft.Toolkit.Mvvm.Input;
+using WOInterface.Core;
+using WOInterface.MVVM.Model;
+
 namespace WOInterface.MVVM.ViewModel
 {
     public class CreateOrderWindowViewModel
     {
-        
+        public Order? Order { get; set; }
+        public ObservableCollection<Dish> Dishes { get; set; }
+
+        public CreateOrderWindowViewModel()
+        {
+            Dishes = new(Service.Db.Dishes.ToList<Dish>());
+        }
+        public RelayCommand<Window> CancelCommand
+        {
+            get
+            {
+                return new RelayCommand<Window>(o =>
+                {
+                    ReturnToMain(o);
+                });
+            }
+        }
+
+        private void ReturnToMain(Window window)
+        {
+            window.Close();
+            App.Current.Windows[0].Show();
+        }
     }
 }
